d3fa7600b5988a640b0605b0c4323f1342d0e927
Hayden Cleaver <rexcleaver90@gmail.com>
2022-07-24 22:55:00 -0400

    movies.js added and functional

---
+++ movies.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const server = express();
+const axios = require('axios');
+const cors = require('cors');
+
+require('dotenv').config();
+server.use(cors());
+
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+
+class Movies {
+  constructor(obj){
+    this.original_title = obj.original_title;
+    this.overview = obj.overview;
+    this.vote_average = obj.vote_average;
+    this.vote_count = obj.vote_count;
+    this.poster_path = obj.poster_path;
+    this.popularity = obj.popularity;
+    this.release_date = obj.release_date;
+  }
+
+}
+
+const handleMovies = (request, response) => {
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${request.query.query}`;
+
+  axios.get(url).then(res => {
+    if (res) {
+      let movieList = res.data.results.map(movieObj => new Movies (movieObj));
+      response.send(movieList);
+
+    }
+  })
+    .catch((error) => {
+      response.status(404).send(`${error}: Movies Not Found!`);
+    });
+};
+
+module.exports = handleMovies;
