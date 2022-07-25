1cc0c8bdd2f56e8e222926d79377e33d18237d35
Hayden Cleaver <rexcleaver90@gmail.com>
2022-07-25 02:59:33 -0400

    initial cache attempt

--- movies.js
+++ movies.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 server.use(cors());
 
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const Cache = require('./cache');
 
 class Movies {
   constructor(obj){
@@ -22,18 +23,35 @@ class Movies {
 }
 
 const handleMovies = (request, response) => {
+  let cacheID = request.query.query;
+  let currentDate = Date.now();
+
   let url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${request.query.query}`;
 
-  axios.get(url).then(res => {
-    if (res) {
-      let movieList = res.data.results.map(movieObj => new Movies (movieObj));
-      response.send(movieList);
-
-    }
-  })
-    .catch((error) => {
-      response.status(404).send(`${error}: Movies Not Found!`);
+  if (Cache[cacheID] && (currentDate - Cache[cacheID].timestamp < 40000 )) {
+    console.log('Hit');
+    return Cache[cacheID].data;
+  } else {
+    console.log('Miss');
+    Cache[cacheID] = {};
+    Cache[cacheID].timestamp = currentDate;
+    Cache[cacheID].data = axios.get(url).then(res => {
+      if (res) {
+        let movieList = res.data.results.map(movieObj => new Movies (movieObj));
+        response.send(movieList);
+      }
     });
+  }
+  // axios.get(url).then(res => {
+  //   if (res) {
+  //     let movieList = res.data.results.map(movieObj => new Movies (movieObj));
+  //     response.send(movieList);
+
+  //   }
+  // })
+  //   .catch((error) => {
+  //     response.status(404).send(`${error}: Movies Not Found!`);
+  //   });
 };
 
 module.exports = handleMovies;
